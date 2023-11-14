4e204f90bc7830956ae417e4e625f90573467d78
gevogdo <p18vogd@ionio.gr>
2023-11-14 19:00:21 +0200

    Project initialization

---
+++ src/test/java/com/example/TrafficProject/TrafficProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrafficProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
