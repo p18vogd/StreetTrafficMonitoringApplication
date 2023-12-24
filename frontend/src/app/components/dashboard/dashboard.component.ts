0dc406a97d9c38b5f61da7128b7d9cbe5cbf7470
gevogdo <p18vogd@ionio.gr>
2023-12-24 16:08:08 +0200

    Frontend dashboard component updated with mini cards and chart

--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -14,9 +14,17 @@ export class DashboardComponent implements OnInit {
   // years: number[];
   data: any;
   chart: any=[];
+
   formattedStartDate!: string;
   formattedEndDate!: string;
   isLoadingData !: boolean;
+
+  //MiniCard variables
+  numberOfVehicles !: string;
+  averageSpeedInRoad !: string;
+  numberOfRoadsInNetwork !: string;
+  showCardFooter !: boolean;
+
 
   constructor(private dashboardService: DashboardService,
               private dateAdapter: DateAdapter<Date>)
@@ -36,6 +44,7 @@ onStartDateChange(selectedDate: MatDatepickerInputEvent<any, any>) {
       this.formattedEndDate = selectedDate.value.toLocaleDateString();
       console.log('Selected End Date:', this.formattedEndDate);
       this.updateChart(this.formattedStartDate, this.formattedEndDate);
+      this.updateMiniCardContent(this.formattedStartDate,this.formattedEndDate);
     }
   }
   ngOnInit(): void {
@@ -43,6 +52,7 @@ onStartDateChange(selectedDate: MatDatepickerInputEvent<any, any>) {
 
   updateChart(startDate: string, endDate: string): void {
     this.isLoadingData = true;
+    this.showCardFooter = false;
     let params = new HttpParams();
     params = params.append('startDate', startDate);
     params = params.append('endDate',endDate);
@@ -69,6 +79,7 @@ onStartDateChange(selectedDate: MatDatepickerInputEvent<any, any>) {
             ],
           },
           options: {
+            responsive: true,
             scales: {
               y: {
                 beginAtZero: true,
@@ -77,6 +88,27 @@ onStartDateChange(selectedDate: MatDatepickerInputEvent<any, any>) {
           },
         });
       this.isLoadingData = false;
+      this.showCardFooter = true;
+    });
+  }
+
+  updateMiniCardContent(startDate: string, endDate: string): void {
+    let params = new HttpParams();
+    params = params.append('startDate', startDate);
+    params = params.append('endDate',endDate);
+    this.dashboardService.getMiniCardData(params).subscribe(data => {
+      this.numberOfVehicles = data[0].toLocaleString("de-DE");
+      this.averageSpeedInRoad = data[1].toString() + 'km/h';
+      this.numberOfRoadsInNetwork = data[2].toString();
+      if(data[0] === 0){
+        this.numberOfVehicles = 'No available data'
+      }
+      if(data[1] === 0){
+        this.averageSpeedInRoad = 'No available data'
+      }
+      if(data[2] === 0){
+        this.numberOfRoadsInNetwork = 'No available data'
+      }
     });
   }
 }
--- frontend/src/app/services/dashboard.service.ts
+++ frontend/src/app/services/dashboard.service.ts
@@ -2,16 +2,21 @@ import { Injectable } from '@angular/core';
 import {HOST, PORT} from "../ApplicationHosts";
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
+import _default from "chart.js/dist/plugins/plugin.tooltip";
+import numbers = _default.defaults.animations.numbers;
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
   private apiUrl = 'http://' + HOST + ':' + PORT + '/getTenHighestCarCountStreets' ;
-  downloadUrl = '';
+  private miniCardUrl = 'http://' + HOST + ':' + PORT + '/getMiniCardData' ;
   constructor(private http: HttpClient) { }
 
   getChartData(requestParams : HttpParams): Observable<TableItem[]> {
     return this.http.get<TableItem[]>(this.apiUrl, {params : requestParams});
+  }
+  getMiniCardData(requestParams : HttpParams): Observable<number[]> {
+    return this.http.get<number[]>(this.miniCardUrl, {params : requestParams});
   }
 }
 export interface TableItem {
